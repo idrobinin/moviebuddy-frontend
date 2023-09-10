ea6242353ce244a9a26ce8b1ff55719b80380828
idrobinin <idrobinin@gmail.com>
2023-09-10 10:36:55 +0800

    Home Component finished

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -4,13 +4,13 @@ defineProps<IMovie>();
 </script>
 
 <template>
-  <div class="rounded-lg px-4 mt-5 min-h-52 h-56 w-96">
+  <div class="rounded-2xl px-4 mt-5 min-h-52 h-56 w-96">
     <img
       :src="poster"
       :alt="title"
-      class="hover:rounded-lg cursor-pointer max-h-40 rounded-lg object-cover h-full w-full shadow-md hover:shadow-zinc-950 hover:scale-105 transition-transform duration-300 transform"
+      class="rounded-2xl cursor-pointer max-h-40 object-cover h-full w-full shadow-md hover:opacity-70 hover:shadow-zinc-950 hover:scale-105 transition-all duration-300 transform"
     />
-    <div class="flex justify-between mt-2.5">
+    <div class="flex justify-between mt-2.5 items-center">
       <h2 class="text-white font-bold">{{ title }}</h2>
       <div class="text-gray-500">
         <span class="mr-3">{{ year }}</span>
