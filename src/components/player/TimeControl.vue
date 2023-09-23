9f7a6c5fb2a3e7b638b4ff332ddef0f6f8a9b76e
idrobinin <idrobinin@gmail.com>
2023-09-23 16:19:22 +0800

    implemented video progress control

--- src/components/player/TimeControl.vue
+++ src/components/player/TimeControl.vue
@@ -1,5 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
 
-<template></template>
+const props = defineProps<{
+  isActive: boolean;
+  videoDuration: number;
+  currentVideoPosition: number;
+}>();
+
+const emits = defineEmits<{
+  "on-time-change": [value: number];
+}>();
+
+const timeBar = ref<number>(0);
+
+const updateTimeBar = (event: Event) => {
+  const currentTarget = event.target as HTMLInputElement;
+  timeBar.value = +currentTarget.value;
+  emits("on-time-change", timeBar.value);
+};
+
+const skip = () => {};
+
+onMounted(() => {
+  window.addEventListener("keydown", skip);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", skip);
+});
+</script>
+
+<template>
+  <div class="controls" v-bind="$attrs">
+    <time class="text-gray-400">00:00</time>
+
+    <input
+      type="range"
+      class="time-track"
+      :min="0"
+      :max="videoDuration"
+      :value="currentVideoPosition"
+      @change="updateTimeBar"
+    />
+
+    <time class="text-gray-400">00:00</time>
+  </div>
+</template>
 
 <style scoped></style>
--- src/components/player/VideoPlayer.vue
+++ src/components/player/VideoPlayer.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
 import TimeControl from "./TimeControl.vue";
 import VolumeControl from "./VolumeControl.vue";
+import { onMounted, ref } from "vue";
+
+const videoDuration = ref<number>(0);
+const videoCurrentTime = ref<number>(0);
+const video = ref();
+const player = ref<HTMLElement>();
+
+const setVideoData = () => {
+  if (video.value?.readyState) {
+    console.log(video.value.duration);
+    videoDuration.value = video.value?.duration;
+  }
+};
+
+const progress = () => {
+  console.log("PROGRESS", video.value.currentTime);
+  videoCurrentTime.value = video.value.currentTime;
+};
+
+const onTimeChange = (value: number) => {
+  video.value.currentTime = value;
+};
+
+onMounted(() => {
+  video.value.addEventListener("timeupdate", progress);
+});
 </script>
 
 <template>
@@ -13,9 +39,17 @@ import VolumeControl from "./VolumeControl.vue";
       controls
       allowfullscreen
       type="video/mp4"
-      poster=""
+      poster="https://i.ytimg.com/vi/0n7llXgSn4U/maxresdefault.jpg"
+      @loadedmetadata="setVideoData"
+      @progress="progress"
     />
-    <TimeControl />
+    <TimeControl
+      :videoDuration="videoDuration"
+      :currentVideoPosition="videoCurrentTime"
+      :isActive="true"
+      @on-time-change="onTimeChange"
+    />
+    {{ videoCurrentTime }}
     <VolumeControl />
   </div>
 </template>
@@ -28,7 +62,7 @@ import VolumeControl from "./VolumeControl.vue";
   @apply rounded-2xl drop-shadow-lg aspect-video block w-full h-full cursor-pointer bg-basic-black;
 }
 
-video::-webkit-media-controls {
-  display: none !important;
-}
+/* video::-webkit-media-controls {
+ display: none !important;
+}*/
 </style>
