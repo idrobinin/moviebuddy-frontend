e5ee4f9cbc43c96d2167a3e1228fcdd386ad41be
idrobinin <idrobinin@gmail.com>
2023-10-31 15:09:00 +0800

    finished volumeControl Comp

--- src/components/player/VolumeControl.vue
+++ src/components/player/VolumeControl.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import { SpeakerWaveIcon, SpeakerXMarkIcon } from "@heroicons/vue/24/solid";
 
 const volumeLevel = ref(50);
@@ -8,9 +8,53 @@ const emits = defineEmits<{
   "on-volume-change": [value: number];
 }>();
 
-const adjustVolume = () => {
+const addVolume = () => {
+  const videoVolume =
+    volumeLevel.value + 10 >= 100 ? 100 : volumeLevel.value + 10;
+  volumeLevel.value = videoVolume;
+  emits("on-volume-change", videoVolume / 100);
+};
+const decVolume = () => {
+  const videoVolume =
+    volumeLevel.value - 10 <= 100 ? 0 : volumeLevel.value - 10;
+  volumeLevel.value = videoVolume;
+  emits("on-volume-change", videoVolume / 100);
+};
+
+const adjustVolume = (event: KeyboardEvent | WheelEvent | Event) => {
+  //key
+  if (event instanceof KeyboardEvent) {
+    if (event.code === "ArrowUp") {
+      addVolume();
+      return;
+    }
+    if (event.code === "ArrowDown") {
+      decVolume();
+      return;
+    }
+  }
+  //wheel
+  if (event instanceof WheelEvent) {
+    if (event.deltaY < 0) {
+      addVolume();
+      return;
+    }
+    if (event.deltaY > 0) {
+      decVolume();
+      return;
+    }
+  }
   emits("on-volume-change", volumeLevel.value / 100);
 };
+
+onMounted(() => {
+  window.addEventListener("keydown", adjustVolume);
+  window.addEventListener("wheel", adjustVolume);
+});
+onUnmounted(() => {
+  window.removeEventListener("keydown", adjustVolume);
+  window.removeEventListener("wheel", adjustVolume);
+});
 </script>
 
 <template>
@@ -41,6 +85,10 @@ const adjustVolume = () => {
 <style scoped>
 .volume-container {
   @apply rotate-270 flex gap-2 absolute left-[-20px] bottom-2/4;
+}
+
+.volume-slider {
+  cursor: pointer;
 }
 .volume-container:hover {
 }
