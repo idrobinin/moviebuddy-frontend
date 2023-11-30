9fad5d3e8ef1a60ddc7ecac2f223ef16dced3e1d
idrobinin <idrobinin@gmail.com>
2023-11-30 11:35:29 +0800

    added types to routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,22 @@
 // Composables
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../pages/Home.vue";
 import Movie from "../pages/Movie.vue";
 import Search from "../pages/Search.vue";
+
+type AppRouteRecord = Omit<RouteRecordRaw, "name" | "children"> & {
+  name: string;
+  children?: readonly RouteRecordRaw[];
+};
+
+type GetRouteName<T extends AppRouteRecord> = T extends {
+  children: AppRouteRecord[];
+}
+  ? T["name"] | GetRouteNames<T["children"]>
+  : T["name"];
+type GetRouteNames<T extends readonly AppRouteRecord[]> = GetRouteName<
+  T[number]
+>;
 
 const routes = [
   {
@@ -20,11 +34,14 @@ const routes = [
     name: "search",
     component: Search,
   },
-];
+] as const satisfies readonly AppRouteRecord[];
+
+// type TRoutes = typeof routes;
+// type TRoutesNames = GetRouteNames<TRoutes>;
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: routes,
 });
 
 export default router;
